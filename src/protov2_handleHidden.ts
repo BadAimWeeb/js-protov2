8138edad0c58cc03e420fc65f05d223a874740f1
BadAimWeeb <badaimweeb@protonmail.com>
2023-02-26 18:37:38 +0700

    handle full OP 0 init

--- src/protov2_handleHidden.ts
+++ src/protov2_handleHidden.ts
@@ -7,6 +7,7 @@ import { decode, encode } from "msgpack-lite";
 import { convertLibp2pStream } from "./stream_wrapper.js";
 
 import debug from "debug";
+import { randomString } from "./utils";
 const log = debug("protov2:hidden_proto");
 
 const TABLE_HIDDEN_PROTOCOL: {
@@ -16,6 +17,7 @@ const TABLE_HIDDEN_PROTOCOL: {
         log(`ProtoV2H 0.1.0 from ${connection.remotePeer.toString()}`);
 
         let handshaked = false;
+        let verificationString = randomString(32);
         let njsStream = convertLibp2pStream(stream);
 
         njsStream.on("data", (d) => {
@@ -33,7 +35,7 @@ const TABLE_HIDDEN_PROTOCOL: {
                                 if (!Object.hasOwn(protov2.config.identifiers, appID)) {
                                     // Check cache
                                     if (Object.hasOwn(protov2._publicCache, appID)) {
-                                        // Send cached data
+                                        // Send cached data with OP 1
                                         njsStream.write(encode({
                                             op: 1,
                                             addresses: [...protov2._publicCache[appID].keys()]
@@ -43,11 +45,51 @@ const TABLE_HIDDEN_PROTOCOL: {
                                         njsStream.write(encode({
                                             op: 5
                                         }));
+                                    }
+                                    njsStream.end();
+                                } else {
+                                    let hasPath = false;
+                                    let hasCache = false;
+
+                                    // Test if we have connections to this appID
+                                    if (Object.hasOwn(protov2._activePath, appID) && protov2._activePath[appID].size > 0) {
+                                        hasPath = true;
+                                    }
+
+                                    // Test if we have cached data for this appID
+                                    if (Object.hasOwn(protov2._publicCache, appID) && protov2._publicCache[appID].size > 0) {
+                                        hasCache = true;
+                                    }
+
+                                    if (hasPath && hasCache) {
+                                        // Return OP 2
+                                        njsStream.write(encode({
+                                            op: 2,
+                                            addresses: [...protov2._publicCache[appID].keys()],
+                                            verify: verificationString
+                                        }));
+                                    } else if (hasPath) {
+                                        // Return OP 3
+                                        njsStream.write(encode({
+                                            op: 3,
+                                            verify: verificationString
+                                        }));
+                                    } else if (hasCache) {
+                                        // Only send cached data with OP 1
+                                        njsStream.write(encode({
+                                            op: 1,
+                                            addresses: [...protov2._publicCache[appID].keys()]
+                                        }));
+                                        njsStream.end();
+                                    } else {
+                                        // Return OP 4
+                                        njsStream.write(encode({
+                                            op: 4
+                                        }));
                                         njsStream.end();
                                     }
-                                } else {
-                                    
                                 }
+                                break;
                         }
                     }
                 }
